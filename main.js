6f2a2e4a22a0184c7f00a54bcca6798315c7134c
notPlancha <andre.plancha@hotmail.com>
2021-05-08 18:03:59 +0100

    amend

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function getContextItens() {
   var itensDiv = document.getElementsByClassName("context-menu-buttons");
-  
+  var newDiv = document.createElement("div");
+  var newbutton = document.createElement("button");
+  newbutton.classList.add("list");
+  newdiv.append(newbutton);
+  itensDiv.append(newdiv);
 }
