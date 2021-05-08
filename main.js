78719c2759c029446bf16af108148bedff07d740
notPlancha <andre.plancha@hotmail.com>
2021-05-09 00:27:15 +0100

    Started js

--- main.js
+++ main.js
@@ -1,8 +1,62 @@
-function getContextItens() {
-  var itensDiv = document.getElementsByClassName("context-menu-buttons");
-  var newDiv = document.createElement("div");
-  var newbutton = document.createElement("button");
-  newbutton.classList.add("list");
-  newdiv.append(newbutton);
-  itensDiv.append(newdiv);
-}
+//Pre loading
+$(document).ready(
+  function() {
+    $('button[name="Refresh"]').click(
+      function() {
+        location.reload();
+      }
+    )
+  }
+);
+
+$(document).ready(
+  function() {
+    $('button[name="Save"]').click(
+      function() {
+        location.reload();
+      }
+    )
+  }
+);
+
+$(document).ready(
+  function() {
+    $('button[name="Load"]').click(
+      function() {
+        location.reload();
+      }
+    )
+  }
+);
+
+$(document).ready(
+  function() {
+    $('button[name="Merge"]').click(
+      function() {
+        location.reload();
+      }
+    )
+  }
+);
+
+//loading
+$(document).ready(
+  function() {
+    $(".context-menu-buttons")
+      .append('<a><div class="list"></div></a>');
+    $(".context-menu-buttons")
+      .append('<a><div class="new list">+</div></a>');
+  }
+);
+
+
+//post-loading
+$(document).ready(
+  function() {
+    $('div.new').click(
+      function() {
+        $('<a><div class="list">Untitled</div></a>').insertBefore(".new")
+      }
+    )
+  }
+);
